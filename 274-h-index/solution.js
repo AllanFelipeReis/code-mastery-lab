0eb0721e6b0ecb7cccf38346fbcaf3cd471b6426
Allan Reis <allanreis3006@gmail.com>
2024-08-21 13:10:26 -0300

    kata: H Index

---
+++ 274-h-index/solution.js
@@ -0,0 +1,28 @@
+/**
+ * @param {number[]} citations
+ * @return {number}
+ */
+var hIndex = function (citations) {
+    const n = citations.length;
+    const count = new Array(n + 1).fill(0);
+
+    for (let c of citations) {
+        if (c >= n) {
+            count[n]++;
+        } else {
+            count[c]++;
+        }
+    }
+
+    let total = 0;
+    for (let i = n; i >= 0; i--) {
+        total += count[i];
+        if (total >= i) {
+            return i;
+        }
+    }
+
+    return 0;
+};
+
+console.log(hIndex([3, 0, 6, 1, 5]));
